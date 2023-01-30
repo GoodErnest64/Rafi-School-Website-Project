36d42b6d957a56dc67dc1b4ad316557fb849f1a5
GoodErnest64 <97891253+GoodErnest64@users.noreply.github.com>
2023-01-30 23:16:42 +0330

    python interaction

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 from flask import *
+import sqlite3
 
 app = Flask(__name__)
 app.secret_key = "WhyNotSetTheSecretKeyToSarina?"
 
+def connect_to_db():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_post(post_id):
+    conn = connect_to_db()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
 @app.route("/")
 def index():
-    return render_template("index.html", blogs=[1, 2, 3])
+    conn = connect_to_db()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
 
 if __name__ == "__main__":
     app.run(debug=True, port=12345)
